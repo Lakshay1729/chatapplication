dd5575b7e12afdd1664c7d43c015709bfaad3d21
Lakshay1729 <garglakshay56@gmail.com>
2020-07-29 21:40:06 +0530

    Another Commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-   path('chat/', include('chat.urls')),
+   path('/', include('chat.urls')),
     path('admin/', admin.site.urls),
 ]
